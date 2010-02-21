5afb40741f44f3fd89a78a5aa185685732f9460a
Nils Gey <denemo@nilsgey.de>
2010-02-21 15:37:32 +0100

    libapr: version bump

--- gub/specs/libapr.py
+++ gub/specs/libapr.py
@@ -2,4 +2,4 @@
 from gub import tools
 
 class Libapr__tools (tools.AutoBuild):
-    source = 'http://apache.cs.uu.nl/dist/apr/apr-1.3.8.tar.gz'
+    source = 'http://apache.cs.uu.nl/dist/apr/apr-1.3.9.tar.gz'
