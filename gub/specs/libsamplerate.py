e35b099e12c77deded4301f6452f4ec449fcf108
Jan Nieuwenhuizen <janneke@gnu.org>
2010-03-03 09:15:29 +0100

    darwin-x86::libsamplerate: add missing libtool dependency.

--- gub/specs/libsamplerate.py
+++ gub/specs/libsamplerate.py
@@ -2,4 +2,8 @@ from gub import target
 
 class Libsamplerate (target.AutoBuild):
     source = 'http://www.mega-nerd.com/SRC/libsamplerate-0.1.7.tar.gz'
-    dependencies = ['tools::automake', 'tools::pkg-config',]
+    dependencies = [
+        'tools::automake',
+        'tools::libtool',
+        'tools::pkg-config',
+        ]
