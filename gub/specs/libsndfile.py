7040caee3951e438f56ba08d7efba34787254a18
Jan Nieuwenhuizen <janneke@gnu.org>
2010-03-03 16:53:39 +0100

    darwin::libsndfile: do not depend on sqlite -- already in SDK.

--- gub/specs/libsndfile.py
+++ gub/specs/libsndfile.py
@@ -2,4 +2,15 @@ from gub import target
 
 class Libsndfile (target.AutoBuild):
     source = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.21.tar.gz'
-    dependencies = ['tools::automake', 'tools::pkg-config', 'libtool', 'sqlite']
+    dependencies = [
+        'tools::automake',
+        'tools::pkg-config',
+        'libtool',
+        'sqlite'
+        ]
+
+class Libsndfile__darwin (Libsndfile):
+    dependencies = [x for x in Libsndfile.dependencies
+                if x.replace ('-devel', '') not in [
+                'sqlite', # Included in darwin-sdk, hmm?
+                ]]
