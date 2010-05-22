ebf5d87e42cf97001b1089d3a3ab683e50b25a65
Jan Nieuwenhuizen <janneke@gnu.org>
2010-05-22 10:56:59 +0200

    cygwin: texlive work.

--- gub/specs/cygwin/texlive.py
+++ gub/specs/cygwin/texlive.py
@@ -1,10 +1,15 @@
 #
 from gub import cygwin
 from gub import gup
+from gub import loggedos
 from gub import misc
+from gub import target
 from gub.specs import texlive
 
 class Texlive (texlive.Texlive):
+    patches = texlive.Texlive.patches + [
+        'texlive-kpathsea-dll.patch',
+        ]
     config_cache_overrides = texlive.Texlive.config_cache_overrides + '''
 xdvi_cv_bitmap_type=${xdvi_cv_bitmap_type='BMTYPE=int BMBYTES=4'}
 xdvi_cv_func_poll=${xdvi_cv_func_poll=yes}
@@ -17,19 +22,32 @@ ac_cv_func_vfork_works=${ac_cv_func_vfork_works=yes}
 xdvi_cv_setsid_in_vfork=${xdvi_cv_setsid_in_vfork=yes}
 lt_cv_cc_dll_switch=${lt_cv_cc_dll_switch="-Wl,--dll -nostartfiles"}
 '''
-    dependencies = gup.gub_to_distro_deps (texlive.Texlive.dependencies,
+    dependencies = gup.gub_to_distro_deps (texlive.Texlive.dependencies + [
+            'libncurses-devel'],
                                            cygwin.gub_to_distro_dict)
+    configure_flags = (target.AutoBuild.configure_flags
+                       + '%(common_configure_flags)s'
+                       + misc.join_lines ('''
+--with-x
+--with-mf-x-toolkit=xaw
+--with-xdvi-x-toolkit=xaw
+--x-includes=%(system_prefix)s/X11R6/include
+--x-libraries=%(system_prefix)s/X11R6/lib
+''')
+                       + "CFLAGS='-O2 -g -DKPSE_DLL'")
     def patch (self):
+        texlive.Texlive.patch (self)
         # FIXME: duh: cross-compile auto-enables t1lib
-        for i in self.locate_files ('%(srcdir)s', 'configure'):
-            self.file_sub ([
+        def defer (logger, i):
+            loggedos.file_sub (logger, [
                     ('ac_compile_t1lib=1', 'ac_compile_t1lib=0'),
                     ('-lpng ', '-lpng12 '),
                     ('-lpng"', '-lpng12"'),
                     ], i)
+        self.map_find_files (defer, '%(srcdir)s', 'configure')
         self.file_sub ([('^(/\* kpsewhich --)', '#undef KPSE_DLL\n\\1')],
                        '%(srcdir)s/texk/kpathsea/kpsewhich.c')
-    make_flags = (texlive.Texlive.make_flags
+    xmake_flags = (texlive.Texlive.make_flags
                 + misc.join_lines ('''
 CFLAGS='-O2 -g -DKPSE_DLL'
 '''))
