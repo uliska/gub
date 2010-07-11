c7746862c89b2a3c192efeef460eeb9446f653b1
Patrick McCarty <pnorcks@gmail.com>
2010-07-10 22:30:29 -0700

    Windows LilyPad: update version.

--- gub/specs/lilypad.py
+++ gub/specs/lilypad.py
@@ -2,24 +2,10 @@ from gub import misc
 from gub import target
 
 class LilyPad (target.AutoBuild):
-    source = 'http://lilypond.org/download/gub-sources/lilypad-0.0.7-1-src.tar.bz2'
+    source = 'http://lilypond.org/download/gub-sources/lilypad-0.0.8-1-src.tar.bz2'
     destdir_install_broken = True
-    # FIXME: better fix Makefile
-    make_flags = misc.join_lines ('''
-ALL_OBJS='$(OBJS)'
-WRC=%(cross_prefix)s/bin/%(target_architecture)s-windres
-CPPFLAGS=-I%(system_prefix)s/include
-RC='$(WRC) $(CPPFLAGS)'
-LIBWINE=
-LIBPORT=
-MKINSTALLDIRS=%(srcdir)s/mkinstalldirs
-INSTALL_PROGRAM=%(srcdir)s/install-sh
-''')
-    def patch (self):
-        ## windres doesn't handle --nostdinc
-        self.file_sub ([('--nostdinc',' '),
-                (r'rc\.res:', r'rc.res.o:')],
-               "%(srcdir)s/Make.rules.in")
+    # FIXME: -DUNICODE is broken, notably for opening files.
+    make_flags = ' CPPFLAGS=-UUNICODE'
     license_files = ['']
 
 Lilypad = LilyPad
