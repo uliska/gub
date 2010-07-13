04211e00a1d25d037509d254e2b80e5ca26882b7
Jan Nieuwenhuizen <janneke@gnu.org>
2010-07-13 21:38:38 +0200

    setuptools: new package.

---
+++ gub/specs/setuptools.py
@@ -0,0 +1,6 @@
+from gub import tools
+import sys
+
+class Setuptools (tools.PythonBuild):
+    source = 'http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz'
+    install_command = 'cd %(srcdir)s && ' + sys.executable + ' %(srcdir)s/setup.py install --prefix=%(tools_prefix)s --root=%(install_root)s'
