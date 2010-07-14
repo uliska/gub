8aac5be10bcfd57346f7a701757e21500d9546a6
Jan Nieuwenhuizen <janneke@gnu.org>
2010-07-14 09:12:14 +0200

    mingw::pygtk: new package.

---
+++ gub/specs/pygtk.py
@@ -0,0 +1,33 @@
+from gub import target
+
+class Pygtk (target.AutoBuild):
+    source = 'http://ftp.gnome.org/pub/GNOME/sources/pygtk/2.16/pygtk-2.16.0.tar.gz'
+    #source = 'http://ftp.gnome.org/pub/GNOME/sources/pygtk/2.17/pygtk-2.17.0.tar.gz'
+    config_cache_overrides = target.AutoBuild.config_cache_overrides + '''
+ac_cv_setwakeupfd_ok=yes
+'''
+    force_autoupdate = True
+    def aclocal_path (self):
+        return (target.AutoBuild.aclocal_path (self)
+                + ['%(srcdir)s/m4', '%(srcdir)s'])
+    configure_command = ('PYTHON_INCLUDES=-I%(system_prefix)s/include/python2.4 '
+                         + target.AutoBuild.configure_command)
+    patches = [
+        'pygtk-cross.patch',
+        ]
+    dependencies = [
+        'python',
+        'pygobject',
+        'gtk+',
+        'pycairo',
+        ]
+
+class Pygtk__mingw (Pygtk):
+    patches = Pygtk.patches + [
+        'pygtk-mingw.patch',
+        ]
+    config_cache_overrides = target.AutoBuild.config_cache_overrides + '''
+ac_cv_setwakeupfd_ok=no
+'''
+    configure_variables = (Pygtk.configure_variables
+                  + ' LDFLAGS="-lpython2.4"'  % locals ())
