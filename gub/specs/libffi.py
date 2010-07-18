85c668e2b6268a4a7c11ad37ec272f242d9f07a2
Jan Nieuwenhuizen <janneke@gnu.org>
2010-07-18 17:14:36 +0200

    libffi: new package.

---
+++ gub/specs/libffi.py
@@ -0,0 +1,17 @@
+from gub import target
+
+class Libffi (target.AutoBuild):
+    source = 'ftp://sourceware.org/pub/libffi/libffi-3.0.9.tar.gz'
+    dependencies = [
+        'tools::automake',
+        'tools::libtool',
+        'tools::pkg-config',
+        ]
+    # huh?
+    install_flags = (target.AutoBuild.install_flags
+                     + """ includesdir='$(includedir)' """ )
+    def install (self):
+        target.AutoBuild.install (self)
+        self.system ('cd %(install_prefix)s && mv lib/libffi-3.0.9/include .')
+        self.system ('cd %(install_prefix)s && rm -rf lib/libffi-3.0.9')
+                
