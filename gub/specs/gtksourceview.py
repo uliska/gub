fb6cc1e4460abefbf101ee01286c3a8887efd88e
Jan Nieuwenhuizen <janneke@gnu.org>
2010-07-18 14:15:24 +0200

    gtksourceview: new package.

---
+++ gub/specs/gtksourceview.py
@@ -0,0 +1,7 @@
+from gub import target
+
+class Gtksourceview (target.AutoBuild):
+    source = 'http://ftp.gnome.org/pub/gnome/sources/gtksourceview/2.11/gtksourceview-2.11.2.tar.gz'
+    dependencies = [
+        'gtk+-devel',
+        ]
