7a05c33c3a31d241aedb5b0f84b50969221d1309
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-05 21:05:19 +0100

    g-wrap: new package.

---
+++ gub/specs/g-wrap.py
@@ -0,0 +1,32 @@
+from gub import target
+from gub import tools
+
+class G_wrap (target.AutoBuild):
+    dependencies = [
+        'guile',
+        'libffi',
+        'tools::g-wrap',
+        ]
+    source = 'http://download.savannah.gnu.org/releases/g-wrap/g-wrap-1.9.13.tar.gz'
+    def configure (self):
+        target.AutoBuild.configure (self)
+        self.file_sub ([('-std=gnu99', '')], '%(builddir)s/guile/g-wrap/Makefile')
+        self.file_sub ([('///', '/')], '%(builddir)s/lib/Makefile', must_succeed=True)
+    parallel_build_broken = True
+    
+class G_wrap__mingw (G_wrap):
+    def configure (self):
+        G_wrap.configure (self)
+        for i in ('%(builddir)s/guile/g-wrap/Makefile',
+                  '%(builddir)s/guile/Makefile', # localtime_r
+                  '%(builddir)s/guile/examples/Makefile', # localtime_r
+                  '%(builddir)s/g-wrap/Makefile',
+                  '%(builddir)s/lib/Makefile'):
+            self.file_sub ([('-Werror', '')], i, must_succeed=True)
+
+class G_wrap__tools (tools.AutoBuild, G_wrap):
+    def configure (self):
+        tools.AutoBuild.configure (self)
+        for i in ('%(builddir)s/guile/g-wrap/Makefile',
+                  '%(builddir)s/lib/Makefile'):
+            self.file_sub ([('-std=gnu99', '')], i)
