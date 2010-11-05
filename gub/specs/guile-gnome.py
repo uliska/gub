7c4c08d68f064cdc96883602e1b7c2788775ef74
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-05 23:18:36 +0100

    guile-gnome: actually build cairo, gtk, libgnomecanvas, pango

--- gub/specs/guile-gnome.py
+++ gub/specs/guile-gnome.py
@@ -10,16 +10,25 @@ class Guile_gnome (target.AutoBuild):
         'gnomecanvas',
         ]
     subpackage_names = ['']
+    def patch (self):
+        target.AutoBuild.patch (self)
+        self.dump ('''guile-gnome-schikkers: atk cairo defs gconf glib gtk libglade libgnomecanvas pango
+guile-gnome-xschikkers: atk cairo defs glib gtk libglade libgnomecanvas pango
+''',
+                   '%(srcdir)s/PACKAGES', mode='a')
     def autoupdate (self):
-        self.system ('''cd %(srcdir)s && scripts/configure-packages guile-gnome-platform''')
-        self.system ('''cd %(srcdir)s && scripts/configure-packages libgnomecanvas''')
-        self.system ('cd %(srcdir)s && NOCONFIGURE=1 sh ./autogen.sh')
+#        self.system ('''cd %(srcdir)s && scripts/configure-packages guile-gnome-platform''')
+#        self.system ('''cd %(srcdir)s && scripts/configure-packages libgnomecanvas''')
+#        for i in ('atk', 'cairo' 'glib', 'gtk', 'libglade', 'libgnomecanvas', 'pango',):
+#            self.system ('''cd %(srcdir)s && scripts/configure-packages ''' + i)
+        self.system ('''cd %(srcdir)s && scripts/configure-packages guile-gnome-schikkers''')
+        self.system ('cd %(srcdir)s && NOCONFIGURE=1 ./autogen.sh')
 
     configure_binary = '%(builddir)s/configure'
     def configure (self):
         self.shadow ()
         target.AutoBuild.configure (self)
-        self.file_sub ([('(GUILE_CAIRO_CFLAGS.*cairo/) ', r'\1 %(system_prefix)s/include/guile-cairo')], '%(builddir)s/config.status')
+        self.file_sub ([('(GUILE_CAIRO_CFLAGS.*cairo ) ', r'\1 -I%(system_prefix)s/include/guile-cairo ')], '%(builddir)s/config.status', must_succeed=True)
         self.system ('cd %(builddir)s && ./config.status')
 
 class Guile_gnome__mingw (Guile_gnome):
