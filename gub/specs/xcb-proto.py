fdbe1b63b3bc0c215fbbbb9f9161ade75e24913b
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-05 21:03:12 +0100

    xcb-proto: work with python2.4 too.

--- gub/specs/xcb-proto.py
+++ gub/specs/xcb-proto.py
@@ -2,4 +2,12 @@ from gub import target
 
 class Xcb_proto (target.AutoBuild):
     source = 'http://xcb.freedesktop.org/dist/xcb-proto-1.3.tar.gz'
-    dependencies = ['tools::libtool']
+    dependencies = [
+        'tools::libtool',
+#        'tools::python-2-6',
+        'tools::python',
+        ]
+    def patch (self):
+        self.file_sub ([('2[.]5', '2.4')], '%(srcdir)s/configure.ac', must_succeed=True)
+        self.file_sub ([('2[.]5', '2.4')], '%(srcdir)s/configure', must_succeed=True)
+    #force_autoupdate = True
