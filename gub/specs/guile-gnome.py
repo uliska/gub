4f94d5ec404b8bf8672fd7a7be91ffa482ea1aca
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-05 22:41:15 +0100

    guile-gnome: depend on gnomecanvas and guile-cairo fix.

--- gub/specs/guile-gnome.py
+++ gub/specs/guile-gnome.py
@@ -2,12 +2,14 @@ from gub import target
 from gub import tools
 
 class Guile_gnome (target.AutoBuild):
+    source = 'git://git.sv.gnu.org/guile-gnome.git'
     dependencies = [
         'guile-cairo',
         'gtk+',
         'g-wrap',
+        'gnomecanvas',
         ]
-    source = 'git://git.sv.gnu.org/guile-gnome.git'
+    subpackage_names = ['']
     def autoupdate (self):
         self.system ('''cd %(srcdir)s && scripts/configure-packages guile-gnome-platform''')
         self.system ('''cd %(srcdir)s && scripts/configure-packages libgnomecanvas''')
@@ -17,6 +19,8 @@ class Guile_gnome (target.AutoBuild):
     def configure (self):
         self.shadow ()
         target.AutoBuild.configure (self)
+        self.file_sub ([('(GUILE_CAIRO_CFLAGS.*cairo/) ', r'\1 %(system_prefix)s/include/guile-cairo')], '%(builddir)s/config.status')
+        self.system ('cd %(builddir)s && ./config.status')
 
 class Guile_gnome__mingw (Guile_gnome):
     patches = [
