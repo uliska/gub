bd302d2b2dc177949f11ec16ab584af9f296c784
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-09 12:44:03 +0100

    gconf: new package.

---
+++ gub/specs/gconf.py
@@ -0,0 +1,9 @@
+from gub import gnome
+from gub import target
+
+class Gconf (target.AutoBuild):
+    source = 'http://ftp.gnome.org/pub/GNOME/platform/2.26/2.26.3/sources/GConf-2.26.2.tar.gz'
+    dependencies = [
+        'tools::libtool',
+        'glib-devel',
+        ]
