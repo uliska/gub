7369f54b671e4086db58276af7869a6684bef934
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-09 12:44:51 +0100

    libart: actually build something.

--- gub/specs/libart.py
+++ gub/specs/libart.py
@@ -5,9 +5,9 @@ class Libart (target.AutoBuild):
     source = 'http://ftp.gnome.org/pub/GNOME/platform/2.26/2.26.3/sources/libart_lgpl-2.3.20.tar.gz'
     dependencies = [
         'tools::libtool',
-#        'tools::intltool',
         'glib-devel',
-#        'gtk+-devel',
-#        'pangocairo-devel',
-#        'libart-devel',
         ]
+    def compile (self):
+        self.system ('cd %(builddir)s && make CC=gcc CPPFLAGS=-I. gen_art_config')
+        self.system ('cd %(builddir)s && cp %(srcdir)s/libart.def .')
+        target.AutoBuild.compile (self)
