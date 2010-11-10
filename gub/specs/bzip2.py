03816fe04f0dcea0e1671683ba6abf3a4236fcf1
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-10 15:00:40 +0100

    target::bzip: new package.

--- gub/specs/bzip2.py
+++ gub/specs/bzip2.py
@@ -1,4 +1,16 @@
 from gub import tools
+from gub import target
+
+class Bzip2 (target.MakeBuild):
+    source = 'http://www.bzip.org/1.0.5/bzip2-1.0.5.tar.gz'
+    compile_flags = ''' -f Makefile-libbz2_so CC='%(toolchain_prefix)sgcc %(target_gcc_flags)s -fno-stack-protector' '''
+    install_flags = (target.MakeBuild.install_flags
+                     + ' PREFIX=%(install_prefix)s')
+    def install (self):
+        target.MakeBuild.install (self)
+        self.system ('cp -pv %(builddir)s/libbz2.so* %(install_prefix)s/lib')
+        # junk broken symlinks
+        self.system ('cd %(install_prefix)s/bin && rm -f bzless bzfgrep bzegrep bzcmp')
 
 class Bzip2__tools (tools.MakeBuild):
     source = 'http://www.bzip.org/1.0.5/bzip2-1.0.5.tar.gz'
