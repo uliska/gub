70a076d8195aa30a9275319938c67fc5b36e3e0b
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-10 15:01:07 +0100

    gconf: build update.

--- gub/specs/gconf.py
+++ gub/specs/gconf.py
@@ -5,5 +5,7 @@ class Gconf (target.AutoBuild):
     source = 'http://ftp.gnome.org/pub/GNOME/platform/2.26/2.26.3/sources/GConf-2.26.2.tar.gz'
     dependencies = [
         'tools::libtool',
-        'glib-devel',
+        'orbit',
+        'dbus-glib',
         ]
+    configure_flags = target.AutoBuild.configure_flags + ' --disable-defaults-service '
