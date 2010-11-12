652a2bfb90f1f56b2fd4dda0a8b849c59fe9364f
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-12 12:51:06 +0100

    guile-cairo: depend on cairo.  Duh.

--- gub/specs/guile-cairo.py
+++ gub/specs/guile-cairo.py
@@ -3,6 +3,7 @@ from gub import target
 class Guile_cairo (target.AutoBuild):
     source = 'http://download.gna.org/guile-cairo/guile-cairo-1.4.0.tar.gz'
     dependencies = [
+        'cairo',
         'guile',
         ]
     subpackage_names = ['']
