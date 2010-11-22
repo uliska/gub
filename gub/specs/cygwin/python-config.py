b81c7cdc60f62cb7c61a8ec06309ebf499e5042d
Jan Nieuwenhuizen <janneke@gnu.org>
2010-11-22 11:12:26 +0100

    cygwin::python-config: update to 2.6.

--- gub/specs/cygwin/python-config.py
+++ gub/specs/cygwin/python-config.py
@@ -3,3 +3,6 @@ python_config = misc.load_spec ('python-config')
 
 class Python_config (python_config.Python_config):
     source = 'url://host/python-config-2.5.tar.gz'
+
+class Python_config__cygwin (python_config.Python_config):
+    source = 'url://host/python-config-2.6.tar.gz'
