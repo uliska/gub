a4a2f2ad787514a5ba2d2f6c73924fb94b46d2be
Jan Nieuwenhuizen <janneke@gnu.org>
2010-12-07 14:50:55 +0100

    mingw::licoli: also install test executable.

--- gub/specs/licoli.py
+++ gub/specs/licoli.py
@@ -9,3 +9,6 @@ class Licoli__mingw (Licoli):
     def compile (self):
         Licoli.compile (self)
         self.system ('cd %(builddir)s && make -C src test.exe')
+    def install (self):
+        Licoli.install (self)
+        self.system ('cp -pv %(builddir)s/src/test.exe %(install_prefix)s/bin/test-licoli.exe')
