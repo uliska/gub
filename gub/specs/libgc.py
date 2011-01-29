cbb35e53e4be54564c796f495c2c9d10d8b29a83
Jan Nieuwenhuizen <janneke@gnu.org>
2011-01-29 15:46:03 +0100

    mingw::libgc: include pthreads in build.

--- gub/specs/libgc.py
+++ gub/specs/libgc.py
@@ -1,7 +1,9 @@
 from gub import target
 
 class Libgc (target.AutoBuild):
-    source = 'http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc-7.1.tar.gz'
+    configure_flags = target.AutoBuild.configure_flags + ' --enable-threads=pthreads'
+    #source = 'http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc-7.1.tar.gz'
+    source = 'http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc6.8.tar.gz&version=6.8'
 
 class Libgc__freebsd (Libgc):
     make_flags = 'THREADDLLIBS=-pthread'
@@ -11,5 +13,11 @@ class Libgc__freebsd__x86 (Libgc__freebsd):
     #patches = ['libgc-6.8-freebsd-x86_64.patch']
 
 class Libgc__mingw (Libgc):
+    dependencies = [
+        'pthreads-w32',
+        ]
+    patches = [
+        'libgc-6.8-mingw-pthreads.patch',
+        ]
+    force_autoupdate = True
     source = 'http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc6.8.tar.gz&version=6.8'
-    #patches = ['libgc-6.8-freebsd-x86_64.patch']
