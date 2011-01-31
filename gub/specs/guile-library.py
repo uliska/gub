0eee742c1098f5a18cacc380bf36bc2eb0b3d944
Jan Nieuwenhuizen <janneke@gnu.org>
2011-01-31 15:02:50 +0100

    guile-library: bump to 1.9.

--- gub/specs/guile-library.py
+++ gub/specs/guile-library.py
@@ -1,7 +1,7 @@
 from gub import target
 
 class Guile_library (target.AutoBuild):
-    source = 'http://download.gna.org/guile-lib/guile-lib-0.1.6.tar.gz'
+    source = 'http://download.savannah.gnu.org/releases/guile-lib/guile-lib-0.1.9.tar.gz'
     dependencies = [
         'guile',
         ]
