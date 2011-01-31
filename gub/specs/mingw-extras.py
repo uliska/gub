a3d10a021a25e0032dd14d42ebe94ce0e8e68286
Jan Nieuwenhuizen <janneke@gnu.org>
2011-01-31 15:03:06 +0100

    mingw-extras: bump to 0.3.

--- gub/specs/mingw-extras.py
+++ gub/specs/mingw-extras.py
@@ -1,6 +1,8 @@
 from gub import target
 
 class Mingw_extras (target.AutoBuild):
-    source = 'http://lilypond.org/download/gub-sources/mingw-extras-0.1.tar.gz'
-    dependencies = ['tools::libtool']
+    source = 'http://lilypond.org/download/gub-sources/mingw-extras-0.3.tar.gz'
+    dependencies = [
+        'tools::libtool',
+        ]
     subpackage_names = ['']
