2a0ca205b922d638dfc8db13011b236b5eb4fe2a
Jan Nieuwenhuizen <janneke@gnu.org>
2011-02-01 15:18:22 +0100

    mingw::guile-glib-spawn: move dl-openable .la to bin.  Fixes loading module.

--- gub/specs/guile-glib-spawn.py
+++ gub/specs/guile-glib-spawn.py
@@ -8,3 +8,9 @@ class Guile_glib_spawn (target.AutoBuild):
         'tools::libtool',
         ]
     subpackage_names = ['']
+
+class Guile_glib_spawn__mingw (Guile_glib_spawn):
+    def install (self):
+        Guile_glib_spawn.install (self)
+        # dlopen-able .la files go in BIN dir, BIN OR LIB package
+        self.system ('''mv %(install_prefix)s/lib/libguile-glib-spawn.la %(install_prefix)s/bin''')
