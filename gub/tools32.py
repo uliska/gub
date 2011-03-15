a1026094d01a9cd16dac61988ec833d2393c2db0
Jan Nieuwenhuizen <janneke@gnu.org>
2011-03-15 21:04:50 +0100

    tools32: make separately runnable as platform.

--- gub/tools32.py
+++ gub/tools32.py
@@ -6,6 +6,9 @@ from gub import misc
 def get_cross_build_dependencies (settings):
     return []
 
+def change_target_package (package):
+    tools.package_auto_dependency_dict (package)
+
 class AutoBuild (tools.AutoBuild):
     def get_substitution_dict (self, env={}):
         dict = {
