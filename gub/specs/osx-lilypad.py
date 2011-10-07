c8abc636cd0c7b5d8ddfa9cdd74976ab148f2e39
Graham Percival <graham@percival-music.ca>
2011-10-07 22:35:32 +0100

    Move back to lilypad-0.4 for ppc.

--- gub/specs/osx-lilypad.py
+++ gub/specs/osx-lilypad.py
@@ -4,7 +4,9 @@ class Osx_lilypad (build.NullBuild):
     pass
 
 class Osx_lilypad__darwin__ppc (build.NullBuild):
-    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.5.tar.gz'
+    # yes, we really need the old version here!
+    # 0.5 was built for lion 10.7, which doesn't have any ppc support
+    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.4.tar.gz'
 
 class Osx_lilypad__darwin__x86 (build.NullBuild):
     source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.5.tar.gz'
