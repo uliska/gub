d68db61ba823ff2bb9e07b89d638e859704bdf21
Mike Solomon <mike@apollinemike.com>
2012-01-21 18:03:30 +0000

    Adds new osx-lilypad source

--- gub/specs/osx-lilypad.py
+++ gub/specs/osx-lilypad.py
@@ -4,9 +4,7 @@ class Osx_lilypad (build.NullBuild):
     pass
 
 class Osx_lilypad__darwin__ppc (build.NullBuild):
-    # yes, we really need the old version here!
-    # 0.5 was built for lion 10.7, which doesn't have any ppc support
-    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.4.tar.gz'
+    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.6.tar.gz'
 
 class Osx_lilypad__darwin__x86 (build.NullBuild):
-    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.5.tar.gz'
+    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.6.tar.gz'
