3d0dfa3070d7a31a12911330ea6bff647c9ec50e
Graham Percival <graham@percival-music.ca>
2012-02-17 12:28:57 +0000

    Change osx-lilypad downloads for PPC and x86.

--- gub/specs/osx-lilypad.py
+++ gub/specs/osx-lilypad.py
@@ -4,7 +4,9 @@ class Osx_lilypad (build.NullBuild):
     pass
 
 class Osx_lilypad__darwin__ppc (build.NullBuild):
-    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.6.tar.gz'
+    # yes, we really need the old version here
+    # later versions were built on OSX 10.7, which doesn't have pcc support
+    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal/osx-lilypad-universal-0.4.tar.gz'
 
 class Osx_lilypad__darwin__x86 (build.NullBuild):
-    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal-0.6.tar.gz'
+    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal/osx-lilypad-universal-0.6.1.tar.gz'
