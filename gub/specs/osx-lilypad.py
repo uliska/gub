72a99b3933d8116c40fbcd5b84294938322674cb
Graham Percival <graham@percival-music.ca>
2012-03-02 11:57:09 +0000

    update to newer lilypad

--- gub/specs/osx-lilypad.py
+++ gub/specs/osx-lilypad.py
@@ -9,4 +9,4 @@ class Osx_lilypad__darwin__ppc (build.NullBuild):
     source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal/osx-lilypad-universal-0.4.tar.gz'
 
 class Osx_lilypad__darwin__x86 (build.NullBuild):
-    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal/osx-lilypad-universal-0.6.1.tar.gz'
+    source = 'http://lilypond.org/download/gub-sources/osx-lilypad-universal/osx-lilypad-universal-0.6.3.tar.gz'
