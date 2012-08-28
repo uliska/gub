2a01293d243da1f8126d92fc26ad9163cc284ac3
administrator <administrator@administrator-OptiPlex-760.(none)>
2012-08-28 18:18:30 +0200

    Work around a bashism/optimism breaking dash in texi2dvi shellscript

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -2,7 +2,17 @@ from gub import tools
 
 class Texinfo__tools (tools.AutoBuild):
     source = 'http://ftp.gnu.org/pub/gnu/texinfo/texinfo-4.13a.tar.gz'
+    patches = [
+        'texinfo-texi2dvi-4.13a.patch',
+        ]
     def patch (self):
         tools.AutoBuild.patch (self)
         # Drop ncurses dependency
         self.file_sub ([(' info ',' ')], '%(srcdir)s/Makefile.in')
+    def autoupdate (self):
+        # We don't want to rebuild Texinfo man pages
+        # 
+        tools.AutoBuild.autoupdate (self)
+        self.system ('''
+cd %(srcdir)s && touch doc/*.1
+''')
