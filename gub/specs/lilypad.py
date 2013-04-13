ee6e7bb46c8e6c7bb0c4055dbde1532405a158be
Phil Holmes <mail@philholmes.net>
2013-04-13 11:37:06 +0100

    Update lilypad.py for v0.0.9 of lilypad

--- gub/specs/lilypad.py
+++ gub/specs/lilypad.py
@@ -2,7 +2,7 @@ from gub import misc
 from gub import target
 
 class LilyPad (target.AutoBuild):
-    source = 'http://lilypond.org/download/gub-sources/lilypad-0.0.8-1-src.tar.bz2'
+    source = 'http://lilypond.org/download/gub-sources/lilypad/lilypad-0.0.9-1-src.tar.bz2'
     destdir_install_broken = True
     # FIXME: -DUNICODE is broken, notably for opening files.
     make_flags = ' CPPFLAGS=-UUNICODE'
