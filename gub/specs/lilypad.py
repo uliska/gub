fc1aa0114f22cc668bf936910acb98fc5a900a64
Phil Holmes <mail@philholmes.net>
2014-09-25 13:58:24 +0100

    Add unicode support for LilyPad

--- gub/specs/lilypad.py
+++ gub/specs/lilypad.py
@@ -4,8 +4,6 @@ from gub import target
 class LilyPad (target.AutoBuild):
     source = 'http://lilypond.org/download/gub-sources/lilypad/lilypad-0.1.0.0-src.tar.bz2'
     destdir_install_broken = True
-    # FIXME: -DUNICODE is broken, notably for opening files.
-    make_flags = ' CPPFLAGS=-UUNICODE'
     license_files = ['']
 
 Lilypad = LilyPad
