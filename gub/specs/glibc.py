eef457c5cf7689a4a77d6cc244d3248e4a035a19
Masamichi Hosoda <trueroad@users.noreply.github.com>
2014-10-23 21:24:21 +0900

    Fix binutils version check

--- gub/specs/glibc.py
+++ gub/specs/glibc.py
@@ -49,6 +49,7 @@ class Glibc (target.AutoBuild, cross.AutoBuild):
         'glibc-2.3-slibdir.patch',
         'glibc-2.3-assert-dl_next_tls_modid.patch',
         'glibc-2.3-binutils-2.19-i386.patch',
+        'glibc-2.3-binutils-version-check.patch',
         ]
     dependencies = ['cross/gcc', 'glibc-core', 'tools::bison', 'tools::gzip', 'tools::perl', 'linux-headers']
     configure_flags = (target.AutoBuild.configure_flags + misc.join_lines ('''
