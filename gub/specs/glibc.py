73b2147d1db6f24610070dcee6fd6af6673e5b8e
Masamichi Hosoda <trueroad@users.noreply.github.com>
2014-10-25 23:15:55 +0900

    Fix linux-x86::glibc-core (glibc-2.3) build by gcc-4.8.2
    suppress warning

--- gub/specs/glibc.py
+++ gub/specs/glibc.py
@@ -51,6 +51,7 @@ class Glibc (target.AutoBuild, cross.AutoBuild):
         'glibc-2.3-binutils-2.19-i386.patch',
         'glibc-2.3-binutils-version-check.patch',
         'glibc-2.3-sysdeps-general-initfini.patch',
+        'glibc-2.3-sysdeps-i386-Makefile.patch',
         ]
     dependencies = ['cross/gcc', 'glibc-core', 'tools::bison', 'tools::gzip', 'tools::perl', 'linux-headers']
     configure_flags = (target.AutoBuild.configure_flags + misc.join_lines ('''
