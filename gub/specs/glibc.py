09c3b8ed82b7eb076ea3157a9c28ad0cc122557d
Masamichi Hosoda <trueroad@users.noreply.github.com>
2014-11-12 22:43:08 +0900

    Fix linux-x86::glibc-core (glibc-2.3)
    linuxthreads crti.S

--- gub/specs/glibc.py
+++ gub/specs/glibc.py
@@ -55,6 +55,7 @@ class Glibc (target.AutoBuild, cross.AutoBuild):
         'glibc-2.3-i386-crti-crtn.patch',
         'glibc-2.3-misc-sys-cdefs-inline.patch',
         'glibc-2.3-elf-Makefile.patch',
+        'glibc-2.3-linuxthreads-crti-crtn.patch',
         ]
     dependencies = ['cross/gcc', 'glibc-core', 'tools::bison', 'tools::gzip', 'tools::perl', 'linux-headers']
     configure_flags = (target.AutoBuild.configure_flags + misc.join_lines ('''
