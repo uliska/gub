b0c6864d7c340aba79c4ef9f05526b3b6305f2a9
Masamichi Hosoda <trueroad@users.noreply.github.com>
2014-11-19 20:06:49 +0900

    Fix mingwrt install

--- gub/specs/mingwrt-dev.py
+++ gub/specs/mingwrt-dev.py
@@ -4,9 +4,15 @@ class Mingwrt_dev (build.BinaryBuild, build.SdkBuild):
     source = 'http://downloads.sourceforge.net/project/mingw/MinGW/Base/mingw-rt/mingwrt-3.20/mingwrt-3.20-mingw32-dev.tar.gz&strip=0'
     def install (self):
         self.system ('''
+mkdir -p %(install_prefix)s/include
+mkdir -p %(install_prefix)s/lib
 mkdir -p %(install_prefix)s/share
-cd %(srcdir)s && tar -C %(srcdir)s/ -cf - . | tar -C %(install_prefix)s -xf -
+mkdir -p %(install_prefix)s/doc
+cd %(srcdir)s/include/ && tar -C %(srcdir)s/include/ -cf - . | tar -C %(install_prefix)s/include/ -xf -
+cd %(srcdir)s/lib/ && tar -C %(srcdir)s/lib/ -cf - . | tar -C %(install_prefix)s/lib/ -xf -
+cd %(srcdir)s/share/ && tar -C %(srcdir)s/share/ -cf - . | tar -C %(install_prefix)s/share/ -xf -
+cd %(srcdir)s/doc/ && tar -C %(srcdir)s/doc/ -cf - . | tar -C %(install_prefix)s/share/doc/ -xf -
 mkdir -p %(install_prefix)s/include/sys
 #cp -p %(sourcefiledir)s/mingw-headers/wait.h %(install_prefix)s/include/sys
-mv %(install_prefix)s/doc %(install_root)s/share
+rm -fr %(srcdir)s/include/ %(srcdir)s/lib/ %(srcdir)s/share/ %(srcdir)s/doc/
 ''', locals ())
--- gub/specs/mingwrt-dll.py
+++ gub/specs/mingwrt-dll.py
@@ -7,4 +7,5 @@ class Mingwrt_dll (build.BinaryBuild, build.SdkBuild):
 rm -fr %(srcdir)s/doc/
 mkdir -p %(install_prefix)s/bin/
 cd %(srcdir)s/bin/ && tar -C %(srcdir)s/bin/ -cf - . | tar -C %(install_prefix)s/bin/ -xf -
+rm -fr %(srcdir)s/bin/
 ''', locals ())
