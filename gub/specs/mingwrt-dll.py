f4422016f7d3c9265ddd38247362c67982010d34
Masamichi Hosoda <trueroad@users.noreply.github.com>
2014-11-22 23:31:27 +0900

    Update mingwrt and w32api

--- gub/specs/mingwrt-dll.py
+++ gub/specs/mingwrt-dll.py
@@ -1,7 +1,8 @@
 from gub import build
 
 class Mingwrt_dll (build.BinaryBuild, build.SdkBuild):
-    source = 'http://downloads.sourceforge.net/project/mingw/MinGW/Base/mingw-rt/mingwrt-3.20/mingwrt-3.20-mingw32-dll.tar.gz&strip=0'
+    source = 'http://downloads.sourceforge.net/project/mingw/MinGW/Base/mingw-rt/mingwrt-3.20/mingwrt-3.20-2-mingw32-dll.tar.lzma&strip=0'
+    dependencies = [ 'tools::xzutils' ]
     def install (self):
         self.system ('''
 rm -fr %(srcdir)s/doc/
--- gub/specs/w32api.py
+++ gub/specs/w32api.py
@@ -1,10 +1,13 @@
 from gub import build
 
 class W32api (build.BinaryBuild, build.SdkBuild):
-    #source = 'http://surfnet.dl.sourceforge.net/sourceforge/mingw/mingw-3.12.tar.gz&strip=0'
-    source = 'http://surfnet.dl.sourceforge.net/sourceforge/mingw/w32api-3.12-mingw32-dev.tar.gz&strip=0'
-    def untar (self):
-        build.BinaryBuild.untar (self)
+    source = 'http://sourceforge.net/projects/mingw/files/MinGW/Base/w32api/w32api-3.17/w32api-3.17-2-mingw32-dev.tar.lzma&strip=0'
+    dependencies = [ 'tools::xzutils' ]
+    def install (self):
         self.system ('''
-cd  %(srcdir)s/ && mkdir usr && mv include lib usr/
-''')
+mkdir -p %(install_prefix)s/include
+mkdir -p %(install_prefix)s/lib
+cd %(srcdir)s/include/ && tar -C %(srcdir)s/include/ -cf - . | tar -C %(install_prefix)s/include/ -xf -
+cd %(srcdir)s/lib/ && tar -C %(srcdir)s/lib/ -cf - . | tar -C %(install_prefix)s/lib/ -xf -
+rm -fr %(srcdir)s/include/ %(srcdir)s/lib/
+''', locals ())
