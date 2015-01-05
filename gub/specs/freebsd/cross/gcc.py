e625ace23f7d4200c52fe200713b16f9de3b78af
Masamichi Hosoda <trueroad@users.noreply.github.com>
2015-01-05 21:05:11 +0900

    Fix freebsd-x86::cross/gcc
    libcilkrts cannot be installed.

--- gub/specs/freebsd/cross/gcc.py
+++ gub/specs/freebsd/cross/gcc.py
@@ -8,5 +8,6 @@ class Gcc__freebsd (cross_gcc.Gcc):
     configure_flags = (cross_gcc.Gcc.configure_flags
                 + misc.join_lines ('''
 --program-prefix=%(toolchain_prefix)s
+--disable-libcilkrts
 --disable-__cxa_atexit
 '''))
