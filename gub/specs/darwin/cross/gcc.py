8fa09af058fec5b4f7feb236508c6eedf58cb727
Masamichi Hosoda <trueroad@users.noreply.github.com>
2015-01-19 23:30:30 +0900

    Fix darwin-x86::cross/gcc
    libcilkrts cannot be compiled.
    This commit can success following commands.
    
    bin/gub darwin-x86::lilypond
    bin/gub darwin-x86::lilypond-installer

--- gub/specs/darwin/cross/gcc.py
+++ gub/specs/darwin/cross/gcc.py
@@ -10,6 +10,9 @@ class Gcc__darwin (cross_gcc.Gcc):
         'gcc-4.8.2-darwin-fixinc.patch',
         'gcc-4.8.2-darwin-libgcc.patch',
     ]
+    configure_flags = (cross_gcc.Gcc.configure_flags
+                       + ' --disable-libcilkrts'
+    )
     def languages (self):
         # objective-c is used for quartz's Carbon/Carbon.h in pango, gtk+
         return cross_gcc.Gcc.languages (self) + ['objc', 'obj-c++']
