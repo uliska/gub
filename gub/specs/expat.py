7df907f3b25fff965613de782c99f35427847267
Masamichi Hosoda <trueroad@users.noreply.github.com>
2015-02-21 18:22:27 +0900

    Update expat to 2.1.0
    avoid libexpat version conflict
    when input/regression/musicxml/ tests

--- gub/specs/expat.py
+++ gub/specs/expat.py
@@ -4,26 +4,8 @@ from gub import target
 from gub import tools
 
 class Expat (target.AutoBuild):
-    source = 'http://sourceforge.net/projects/expat/files/expat/2.0.1/expat-2.0.1.tar.gz'
-    patches = ['expat-2.0.1-mingw.patch']
+    source = 'http://sourceforge.net/projects/expat/files/expat/2.1.0/expat-2.1.0.tar.gz'
     dependencies = ['libtool', 'tools::expat']
-    make_flags = misc.join_lines ('''
-CFLAGS='-O2 -DHAVE_EXPAT_CONFIG_H'
-EXEEXT=
-RUN_FC_CACHE_TEST=false
-''')
-    if 'stat' in misc.librestrict (): # 40 lines of sh code from 1993 implementing `mkdir -p', statting components of DESTDIR: /home, ...
-        make_flags = make_flags + ''' 'mkinstalldirs=mkdir -p' '''
-    def patch (self):
-        target.AutoBuild.patch (self)
-        #FIXME: should have configure.ac/in vs configure timestamp test
-        self.system ('rm %(srcdir)s/configure')
-        self.system ('touch %(srcdir)s/tests/xmltest.sh.in')
-
-class Expat__mingw (Expat):
-    # mingw's expat libtool build breaks with DASH
-    configure_variables = (Expat.configure_variables
-                .replace ('SHELL=', 'XSHELL='))
 
 class Expat__linux__arm__vfp (Expat):
     patches = []
