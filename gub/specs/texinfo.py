579a5d79260b27df000fb616e7a38f1ea88cef2b
Masamichi Hosoda <trueroad@users.noreply.github.com>
2015-02-21 22:41:33 +0900

    Update tools::texinfo to 5.2
    avoid texi2dvi bashism
    when input/regression/lilypond-book/ tests

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -1,7 +1,8 @@
 from gub import tools
 
 class Texinfo__tools (tools.AutoBuild):
-    source = 'http://ftp.gnu.org/pub/gnu/texinfo/texinfo-4.13a.tar.gz'
+    source = 'http://ftp.gnu.org/gnu/texinfo/texinfo-5.2.tar.xz'
+    dependencies = [ 'tools::xzutils' ]
     def patch (self):
         tools.AutoBuild.patch (self)
         # Drop ncurses dependency
