6a6b7abb76f1f4603d3bd616f84deafca87f046a
Masamichi Hosoda <trueroad@users.noreply.github.com>
2015-02-22 00:35:13 +0900

    Revert "Update tools::texinfo to 5.2"
    tools::guile building failed.

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -1,8 +1,7 @@
 from gub import tools
 
 class Texinfo__tools (tools.AutoBuild):
-    source = 'http://ftp.gnu.org/gnu/texinfo/texinfo-5.2.tar.xz'
-    dependencies = [ 'tools::xzutils' ]
+    source = 'http://ftp.gnu.org/pub/gnu/texinfo/texinfo-4.13a.tar.gz'
     def patch (self):
         tools.AutoBuild.patch (self)
         # Drop ncurses dependency
