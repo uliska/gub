daae1a23c91b979fc4d8e867d9ba4289c9e694b8
Masamichi Hosoda <trueroad@users.noreply.github.com>
2015-02-22 01:08:12 +0900

    Fix texinfo (texi2dvi) bashism
    for input/regression/lilypond-book/ tests

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -2,7 +2,14 @@ from gub import tools
 
 class Texinfo__tools (tools.AutoBuild):
     source = 'http://ftp.gnu.org/pub/gnu/texinfo/texinfo-4.13a.tar.gz'
+    patches = ['texinfo-4.13a-fix-bashism.patch']
     def patch (self):
         tools.AutoBuild.patch (self)
         # Drop ncurses dependency
         self.file_sub ([(' info ',' ')], '%(srcdir)s/Makefile.in')
+        # Avoid man rebuild
+        self.system('''
+touch %(srcdir)s/doc/texi2dvi.1
+touch %(srcdir)s/doc/texi2pdf.1
+touch %(srcdir)s/doc/pdftexi2dvi.1
+''')
