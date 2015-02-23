e59d069dc68f20ae8a40d9649298ff67d355ebcd
Masamichi Hosoda <trueroad@users.noreply.github.com>
2015-02-23 20:33:34 +0900

    Fix p7zip dependency
    p7zip requires g++.

--- gub/specs/p7zip.py
+++ gub/specs/p7zip.py
@@ -2,7 +2,7 @@ from gub import tools
 
 class P7zip__tools (tools.MakeBuild):
     source = 'http://sourceforge.net/projects/p7zip/files/p7zip/9.20.1/p7zip_9.20.1_src_all.tar.bz2'
-    dependencies = [ 'bzip2' ]
+    dependencies = [ 'bzip2', 'system::g++' ]
     install_flags = ' DEST_HOME=%(install_prefix)s install'
     def patch (self):
         tools.MakeBuild.patch (self)
