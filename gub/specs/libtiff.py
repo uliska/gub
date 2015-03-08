b75bd1c6118c15d48f7f8887f0c144e3101ea73f
Masamichi Hosoda <trueroad@users.noreply.github.com>
2015-03-08 22:52:03 +0900

    Fix libtiff dependency

--- gub/specs/libtiff.py
+++ gub/specs/libtiff.py
@@ -3,12 +3,13 @@ from gub import tools
 
 class Libtiff (target.AutoBuild):
     source = 'http://dl.maptools.org/dl/libtiff/tiff-3.8.2.tar.gz'
-    dependencies = ['tools::libtool', 'libjpeg-devel']
+    dependencies = ['tools::libtool', 'libjpeg-devel', 'zlib']
 
 class Libtiff__tools (tools.AutoBuild, Libtiff):
     dependencies = [
             'libtool',
             'libjpeg-devel',
+            'zlib',
 #            'system::g++',
             ]
     configure_flags = (tools.AutoBuild.configure_flags
