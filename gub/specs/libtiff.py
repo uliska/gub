a6022ad722ddb7c754b168079e6298e4085dfd2d
Masamichi Hosoda <trueroad@users.noreply.github.com>
2015-03-21 21:23:38 +0900

    Add libtiff-runtime subpackage

--- gub/specs/libtiff.py
+++ gub/specs/libtiff.py
@@ -4,6 +4,32 @@ from gub import tools
 class Libtiff (target.AutoBuild):
     source = 'http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz'
     dependencies = ['tools::libtool', 'libjpeg-devel', 'zlib-devel']
+    subpackage_names = ['doc', 'devel', 'runtime', '']
+    def get_subpackage_definitions (self):
+        d = target.AutoBuild.get_subpackage_definitions (self)
+        prefix_dir = self.settings.prefix_dir
+        d['runtime'] = [
+            prefix_dir + '/lib/libtiff*.so*',
+        ]
+        return d
+
+class Libtiff__mingw (Libtiff):
+    def get_subpackage_definitions (self):
+        d = target.AutoBuild.get_subpackage_definitions (self)
+        prefix_dir = self.settings.prefix_dir
+        d['runtime'] = [
+            prefix_dir + '/bin/libtiff*.dll',
+        ]
+        return d
+
+class Libtiff__darwin (Libtiff):
+    def get_subpackage_definitions (self):
+        d = target.AutoBuild.get_subpackage_definitions (self)
+        prefix_dir = self.settings.prefix_dir
+        d['runtime'] = [
+            prefix_dir + '/lib/libtiff*.dylib',
+        ]
+        return d
 
 class Libtiff__tools (tools.AutoBuild, Libtiff):
     dependencies = [
