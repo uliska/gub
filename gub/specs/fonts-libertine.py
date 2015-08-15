9d12fff59e02fdc752e8f55ca7fc70ffdb1ad09c
Masamichi Hosoda <trueroad@trueroad.jp>
2015-08-16 00:53:29 +0900

    Fix fonts-libertine
    Install ttf files only.

--- gub/specs/fonts-libertine.py
+++ gub/specs/fonts-libertine.py
@@ -5,7 +5,7 @@ class Fonts_libertine (build.BinaryBuild):
     source = 'http://sourceforge.net/projects/linuxlibertine/files/linuxlibertine/5.3.0/LinLibertineOTF_5.3.0_2012_07_02.tgz&strip=0'
     def install (self):
         self.system ('mkdir -p %(install_prefix)s/share/fonts/opentype/linux-libertine')
-        self.system ('cp %(srcdir)s/* %(install_prefix)s/share/fonts/opentype/linux-libertine/')
+        self.system ('cp %(srcdir)s/*.otf %(install_prefix)s/share/fonts/opentype/linux-libertine/')
     def package (self):
         build.AutoBuild.package (self)
 
