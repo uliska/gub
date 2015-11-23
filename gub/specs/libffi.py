b3c19c1213c3947501b3eec4f7b771921d8119cd
Masamichi Hosoda <trueroad@trueroad.jp>
2015-11-23 23:24:25 +0900

    Fix linux-64::libffi
    For using /usr/lib instead of /usr/lib64

--- gub/specs/libffi.py
+++ gub/specs/libffi.py
@@ -16,6 +16,12 @@ class Libffi (target.AutoBuild):
         self.system ('cd %(install_prefix)s && mv lib/libffi-3.2.1/include .')
         self.system ('cd %(install_prefix)s && rm -rf lib/libffi-3.2.1')
 
+class Libffi__linux__64 (Libffi):
+    # For using /usr/lib instead of /usr/lib64
+    patches = Libffi.patches + [
+        'libffi-3.2.1-linux-64-libdir.patch',
+    ]
+
 class Libffi__darwin__x86 (Libffi):
     # darwin-x86 can not compile libffi 3.1, 3.2.1.
     source = 'ftp://sourceware.org/pub/libffi/libffi-3.0.13.tar.gz'
