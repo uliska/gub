d7cc6a34685b1889e6dc552764d846e3de4dfa09
Masamichi Hosoda <trueroad@trueroad.jp>
2016-06-02 18:13:15 +0900

    Fix FreeType dependencies etc.

--- gub/specs/freetype.py
+++ gub/specs/freetype.py
@@ -10,8 +10,28 @@ libraries, display servers, font conversion tools, text image generation
 tools, and many other products as well.'''
 
     source = 'http://download.savannah.gnu.org/releases/freetype/freetype-2.6.3.tar.bz2'
-    dependencies = ['libtool-devel', 'zlib-devel', 'tools::bzip2']
+    dependencies = [
+        'libtool-devel',
+        'zlib-devel',
+        'tools::bzip2'
+    ]
     subpackage_names = ['devel', '']
+    configure_flags = (target.AutoBuild.configure_flags
+                       + ' --without-png'
+                       + ' --without-harfbuzz'
+    )
 
 class Freetype__tools (tools.AutoBuild, Freetype):
-    dependencies = ['libtool', 'zlib']
+    dependencies = [
+        'libtool',
+        'zlib',
+        'libpng',
+        'bzip2',
+    ]
+    configure_command = (
+        ''' LIBPNG_CFLAGS='-I%(tools_prefix)s/include/libpng12' ''' +
+        ''' LIBPNG_LIBS='-I%(tools_prefix)s/lib -lpng12' ''' +
+        tools.AutoBuild.configure_command)
+    configure_flags = (tools.AutoBuild.configure_flags
+                       + ' --without-harfbuzz'
+    )
