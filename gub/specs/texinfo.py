fef2baecb497d1812d7ea10e64627b6145cca1cc
Masamichi Hosoda <trueroad@trueroad.jp>
2016-08-18 18:33:02 +0900

    Add tools::texinfo dependencies

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -2,4 +2,4 @@ from gub import tools
 
 class Texinfo__tools (tools.AutoBuild):
     source = 'http://ftp.gnu.org/pub/gnu/texinfo/texinfo-6.1.tar.xz'
-    dependencies = [ 'tools::xzutils' ]
+    dependencies = [ 'tools::xzutils', 'tools::perl' ]
