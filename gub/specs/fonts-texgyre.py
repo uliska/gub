28a9e0fc94ab625ac76ea7bcb0584c9cc8a0e446
Masamichi Hosoda <trueroad@trueroad.jp>
2017-02-18 01:37:15 +0900

    Add install documents for fonts-texgyre

--- gub/specs/fonts-texgyre.py
+++ gub/specs/fonts-texgyre.py
@@ -2,10 +2,12 @@ from gub import tools
 from gub import build
 
 class Fonts_texgyre (build.BinaryBuild):
-    source = 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/whole/tg-2.005otf.zip'
+    source = 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/whole/tg-2.005bas.zip'
     def install (self):
         self.system ('mkdir -p %(install_prefix)s/share/fonts/opentype/texgyre')
-        self.system ('cp %(srcdir)s/*.otf %(install_prefix)s/share/fonts/opentype/texgyre/')
+        self.system ('cp %(srcdir)s/fonts/opentype/public/tex-gyre/*.otf %(install_prefix)s/share/fonts/opentype/texgyre/')
+        self.system ('mkdir -p %(install_prefix)s/share/doc/texgyre')
+        self.system ('cp %(srcdir)s/doc/fonts/tex-gyre/* %(install_prefix)s/share/doc/texgyre/')
     def package (self):
         build.AutoBuild.package (self)
 
